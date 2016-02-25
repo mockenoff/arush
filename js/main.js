1a68cd4c830c18a3190bbb957a46673f428567e2
mockenoff <mockenoff@yahoo.com>
2016-02-25 12:46:50 -0600

    Switch to scroll-based movement for team header

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 var nav = document.querySelector('#nav'),
 	cover = document.querySelector('#cover'),
-	team = document.querySelector('#team'),
 
-	// Team typography nodes
+	// Team section
+	team = document.querySelector('#team'),
+	teamHeader = team.querySelector('h2'),
+	teamHeaderY = {
+		min: -teamHeader.clientHeight,
+		max: 0,
+		active: 50,
+		current: 0,
+	},
 	names = team.querySelectorAll('li'),
 
 	// Display calculation constants
 	WINDOW_HEIGHT = 0,
+	HALF_WINDOW_HEIGHT = 0,
 	TEAM_HEIGHT = team.clientHeight;
 
 function windowResize(ev) {
 	WINDOW_HEIGHT = window.innerHeight;
+	HALF_WINDOW_HEIGHT = WINDOW_HEIGHT / 2;
+	teamHeaderY.max = WINDOW_HEIGHT + Math.abs(teamHeaderY.min);
 }
 window.addEventListener('resize', windowResize);
 windowResize();
@@ -22,16 +32,38 @@ function windowScroll(ev) {
 	nav.style.opacity = Math.min(1, scrollTop / WINDOW_HEIGHT);
 
 	// Determine what is the active section
-	var active = document.querySelector('.splash.active');
-	if (active !== null) {
-		active.classList.remove('active');
+	var newActive = null,
+		oldActive = document.querySelector('.splash.active');
+
+	if (oldActive !== null) {
+		oldActive.classList.remove('active');
 	}
 	if (scrollTop < WINDOW_HEIGHT) {
-		cover.classList.add('active');
+		newActive = cover;
 		history.replaceState({}, '', '/');
 	} else if (scrollTop >= WINDOW_HEIGHT && scrollTop < WINDOW_HEIGHT + TEAM_HEIGHT) {
-		team.classList.add('active');
+		newActive = team;
 		history.replaceState({}, '', '#team');
+	}
+
+	if (newActive === oldActive) {
+		return;
+	}
+	if (newActive !== null) {
+		newActive.classList.add('active');
+	}
+
+	// Do Team title
+	if (newActive === team) {
+		teamHeader.style.opacity = 1;
+		teamHeader.style.top = teamHeaderY.active+'px';
+	} else if (newActive === cover && scrollTop > HALF_WINDOW_HEIGHT) {
+		var opacity = (scrollTop - HALF_WINDOW_HEIGHT) / HALF_WINDOW_HEIGHT;
+		teamHeader.style.opacity = opacity;
+		teamHeader.style.top = teamHeaderY.max - ((teamHeaderY.max - teamHeaderY.active) * opacity)+'px';
+	} else {
+		teamHeader.style.opacity = 0;
+		teamHeader.style.top = teamHeaderY.max+'px';
 	}
 }
 window.addEventListener('scroll', windowScroll);

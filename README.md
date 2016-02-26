11a4b6f3ddad78a6961e8ab1f5bb80a7f301a276
Tim Poon <mockenoff@yahoo.com>
2016-02-25 19:53:02 -0600

    Add team animation setting function

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # arush
+
+Redesign of the [Adrenaline Rush homepage](http://arushdball.com/).
+
+Probably gonna use Gulp or something for a build process.
+
+## Local Development
+
+Just running a command line Python server would be simplest.
+
+```
+$ python -m SimpleHTTPServer 8000
+```
--- js/main.js
+++ js/main.js
@@ -3,21 +3,23 @@ var nav = document.querySelector('#nav'),
 
 	// Team section
 	team = document.querySelector('#team'),
+	teamNames = team.querySelectorAll('li'),
 	teamHeader = team.querySelector('h2'),
 	teamHeaderY = {
 		min: -teamHeader.clientHeight,
 		max: 0,
-		active: 50,
+		active: 100,
 		current: 0,
 	},
-	names = team.querySelectorAll('li'),
 
 	// Display calculation constants
+	WINDOW_WIDTH = 0,
 	WINDOW_HEIGHT = 0,
 	HALF_WINDOW_HEIGHT = 0,
 	TEAM_HEIGHT = team.clientHeight;
 
 function windowResize(ev) {
+	WINDOW_WIDTH = window.innerWidth;
 	WINDOW_HEIGHT = window.innerHeight;
 	HALF_WINDOW_HEIGHT = WINDOW_HEIGHT / 2;
 	teamHeaderY.max = WINDOW_HEIGHT + Math.abs(teamHeaderY.min);
@@ -57,24 +59,44 @@ function windowScroll(ev) {
 	if (newActive === team) {
 		teamHeader.style.opacity = 1;
 		teamHeader.style.top = teamHeaderY.active+'px';
-	} else if (newActive === cover && scrollTop > HALF_WINDOW_HEIGHT) {
-		var opacity = (scrollTop - HALF_WINDOW_HEIGHT) / HALF_WINDOW_HEIGHT;
-		teamHeader.style.opacity = opacity;
-		teamHeader.style.top = teamHeaderY.max - ((teamHeaderY.max - teamHeaderY.active) * opacity)+'px';
+
+		var relativeTop = (scrollTop - WINDOW_HEIGHT) / (TEAM_HEIGHT - WINDOW_HEIGHT);
+		for (var i = 0, l = ANIM.team.length; i < l; i++) {
+			for (var j = 0, k = ANIM.team[i].length; j < k; j++) {
+				if (relativeTop >= ANIM.team[i][j].time[0] && relativeTop < ANIM.team[i][j].time[1]) {
+					setAnimPosition(teamNames[i], ANIM.team[i][j], relativeTop);
+				}
+			}
+		}
+	} else if (newActive === cover) {
+		if (scrollTop > HALF_WINDOW_HEIGHT) {
+			var opacity = (scrollTop - HALF_WINDOW_HEIGHT) / HALF_WINDOW_HEIGHT;
+			teamHeader.style.opacity = opacity;
+			teamHeader.style.top = teamHeaderY.max - ((teamHeaderY.max - teamHeaderY.active) * opacity)+'px';
+		} else {
+			teamHeader.style.opacity = 0;
+			teamHeader.style.top = teamHeaderY.max+'px';
+		}
+		for (var i = 0, l = ANIM.team.length; i < l; i++) {
+			setAnimPosition(teamNames[i], ANIM.team[i][0], 0);
+		}
 	} else {
 		teamHeader.style.opacity = 0;
-		teamHeader.style.top = teamHeaderY.max+'px';
+		teamHeader.style.top = teamHeaderY.min+'px';
+		for (var i = 0, l = ANIM.team.length; i < l; i++) {
+			setAnimPosition(teamNames[i], ANIM.team[i][ANIM.team[i].length - 1], 1);
+		}
 	}
 }
 window.addEventListener('scroll', windowScroll);
 windowScroll();
 
-function hashChange(ev) {
-	console.log('ASDF', ev);
-	if (ev !== undefined) {
-		ev.preventDefault();
-		return false;
-	}
+function setAnimPosition(elem, item, progress) {
+	var relativeTime = (progress - item.time[0]) / (item.time[1] - item.time[0]),
+		scaledDiff = [
+			(item.end[0] - item.start[0]) * relativeTime,
+			(item.end[1] - item.start[1]) * relativeTime,
+		];
+	elem.style.left = ((item.start[0] + scaledDiff[0]) * WINDOW_WIDTH)+'px';
+	elem.style.top = ((item.start[1] + scaledDiff[1]) * WINDOW_HEIGHT)+'px';
 }
-window.addEventListener('hashchange', hashChange);
-hashChange();
